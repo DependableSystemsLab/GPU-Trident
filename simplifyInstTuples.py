bac8a95e37bed99f1193176d2cbb04be95e37183
Abdul Rehman <abdul@karthik-pc00.(none)>
2019-01-03 11:34:32 -0800

    Updated simplifyInstTuples.py to handle instructions that can't be indexed

--- simplifyInstTuples.py
+++ simplifyInstTuples.py
@@ -2,17 +2,29 @@
 
 import os, sys
 
+prev_index = -1
+
 os.system("rm results/simplified_inst_tuples.txt")
 with open("results/inst_tuples.txt" ,'r') as itf:
 	lines = itf.readlines()
 	for line in lines:
 		index = int(line.split(" ")[0])
+		
+		# If an instruction was not indexed add values for it in the tuple file
+		# by setting propagation probability to 1.0
+		if index != (prev_index + 1):
+		    print "Into the darkness\n"
+		    with open("results/simplified_inst_tuples.txt", 'a') as sf:
+		        while ((index - 1) != prev_index):
+		            sf.write(str(prev_index + 1) + " 1.0 0.0 0.0\n")
+		            prev_index += 1
+		else:
+		    print "Nope\n"
+		            
 		pR = float( line.split(" ")[1].split(",")[0].replace("<", "") )
 		mR = float( line.split(" ")[1].split(",")[1] )
 		cR = float( line.split(" ")[1].split(",")[2].replace(">", "").replace("\n", "") )
 		with open("results/simplified_inst_tuples.txt", 'a') as sf:
 			sf.write(`index` + " " + `pR` + " " + `mR` + " " + `cR` + "\n")
 			sf.close()
-
-
-	
+		prev_index = index
