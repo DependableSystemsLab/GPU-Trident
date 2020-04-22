ebe27613eecb5e8bf73cbcd0f4127844e5e94d23
Abdul Rehman <arehman.anwer1@gmail.com>
2020-04-22 12:06:53 -0700

    Minor updates

--- benchmarks/Perf_BM/config.py
+++ benchmarks/Perf_BM/config.py
@@ -6,8 +6,8 @@ INPUT_PARAMETERS = ""
 LLVM_PATH = ""
 EXEC_MODE = 1 # 0 -> Single threaded, 1 -> Multi-threaded
 
-CF_STAGE_1_NUM = 100
-CF_STAGE_2_NUM = 100
+CF_STAGE_1_NUM = 200
+CF_STAGE_2_NUM = 200
 
 # Loads that transfer data from global memory
 GLOBAL_LOAD_LIST = [] #K1
